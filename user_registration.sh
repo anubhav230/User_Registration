b18cf5b412e16c952ed7bb1f8aa4bd783a1a3690
anubhav230 <anubhavsingh230@gmail.com>
2020-05-06 12:43:51 +0530

    last name pattern

--- user_registration.sh
+++ user_registration.sh
@@ -1,9 +1,21 @@
 #!/bin/bash -x
+
 read -p "Enter first name: " firstName
-firstNamePattern="^[A-za-z]{3,10}$"
+firstNamePattern="^[A-z]{1}[a-z]{2,10}$"
 if [[ $firstName =~ $firstNamePattern ]]
 then
-	echo "done"
+        echo "done"
 else
-	echo "invalid name"
+        echo "invalid first name"
+
+fi
+
+read -p "Enter last name: " lastName
+lastNamePattern="^[A-Z]{1}[a-z]{2,10}$"
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo "done"
+else
+        echo "invalid last name"
 fi
