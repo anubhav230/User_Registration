ba280b5195fa1ff14c105793d4d77aec0eb439ff
anubhav230 <63615228+anubhav230@users.noreply.github.com>
2020-05-06 14:28:09 +0530

    first name

--- user_registration.sh
+++ user_registration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 read -p "Enter first name: " firstName
-firstNamePattern="^[A-za-z]{3,10}$"
+firstNamePattern="^[A-z]{1}[a-z]{2,10}$"
 if [[ $firstName =~ $firstNamePattern ]]
 then
 	echo "done"
