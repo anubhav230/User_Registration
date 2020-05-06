44567658c4da33a4edff1dc3a6fc9eed98db4f76
anubhav230 <anubhavsingh230@gmail.com>
2020-05-07 00:14:17 +0530

    email pattern

--- user_registration.sh
+++ user_registration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
-read -p "Enter password: " password
-pwdPattern="^[0-9 a-z A-Z \! \@ \#\$\%\^\&\*]{8}$"
-if [[ $password =~ $pwdPattern ]]
+read -p "Enter email id: " email
+emailPattern="^[0-9a-zA-Z]+([. _ + -][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+if [[ $email =~ $emailPattern ]]
 then
         echo "valid"
 else
